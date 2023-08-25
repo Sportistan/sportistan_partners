5607434f2824f8af11411313682232d79d4c7864
HP <Amrit.randhawa.ji@gmail.com>
2023-08-25 16:43:52 +0530

    25 Aug

---
+++ lib/login/authentication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({super.key});
+
+  @override
+  State<Authentication> createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
